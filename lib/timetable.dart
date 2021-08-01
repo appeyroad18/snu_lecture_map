a6e1faf4e4195cb70fd01b58661c9a502bca69c5
daeun1234! <de0330@naver.com>
2021-08-01 13:17:37 +0900

    Update timetable.dart

--- lib/timetable.dart
+++ lib/timetable.dart
@@ -12,8 +12,9 @@ class TimeTable extends StatefulWidget {
 class _TimeTableState extends State<TimeTable> {
   @override
   Widget build(BuildContext context) {
+    BoxSize _boxSize = Provider.of<BoxSize>(context);
     return
-        ChangeNotifierProvider(
+      ChangeNotifierProvider(
         create: (BuildContext context) => BoxSize(),
         child:
         Scaffold(
@@ -44,9 +45,7 @@ class _TimeTableState extends State<TimeTable> {
                   icon: Icon(Icons.settings),
                   color: Colors.black,
                   onPressed: () {
-                    //Container(color: Colors.yellow,),
-                    //Container(color: Colors.red,)],);
-                    print('Setting button is clicked');
+                    _boxSize.delete_all();
                   },
                 ),
               ],
@@ -105,8 +104,8 @@ class _TimeTableState extends State<TimeTable> {
             body: SingleChildScrollView(
               child: _Table(),
             )
-            ),
-            );
+        ),
+      );
   }
 
   void _onButtonPressed() {
@@ -251,49 +250,49 @@ class _TimeSelectState extends State<TimeSelect> {
     showCupertinoModalPopup(
         context: ctx,
         builder: (_) => Container(
-              height: 200,
-              color: Color.fromARGB(255, 255, 255, 255),
-              child: Column(
-                children: [
-                  Container(
-                    height: 130,
-                    child: CupertinoPicker(
-                      onSelectedItemChanged: (val) {
-                        setState(() {
-                          //_chosenDateTime = val;
-                          if (val == 0) {
-                            _chosenDateTime = '월요일';
-                          } else if (val == 1) {
-                            _chosenDateTime = '화요일';
-                          } else if (val == 2) {
-                            _chosenDateTime = '수요일';
-                          } else if (val == 3) {
-                            _chosenDateTime = '목요일';
-                          } else if (val == 4) {
-                            _chosenDateTime = '금요일';
-                          }
-                        });
-                      },
-                      itemExtent: 32,
-                      children: [
-                        Text('월'),
-                        Text('화'),
-                        Text('수'),
-                        Text('목'),
-                        Text('금')
-                      ],
-                    ),
-                  ),
-
-                  // Close the modal
-                  CupertinoButton(
-                    child: Text('OK'),
-                    padding: EdgeInsets.zero,
-                    onPressed: () => Navigator.of(ctx).pop(),
-                  )
-                ],
+          height: 200,
+          color: Color.fromARGB(255, 255, 255, 255),
+          child: Column(
+            children: [
+              Container(
+                height: 130,
+                child: CupertinoPicker(
+                  onSelectedItemChanged: (val) {
+                    setState(() {
+                      //_chosenDateTime = val;
+                      if (val == 0) {
+                        _chosenDateTime = '월요일';
+                      } else if (val == 1) {
+                        _chosenDateTime = '화요일';
+                      } else if (val == 2) {
+                        _chosenDateTime = '수요일';
+                      } else if (val == 3) {
+                        _chosenDateTime = '목요일';
+                      } else if (val == 4) {
+                        _chosenDateTime = '금요일';
+                      }
+                    });
+                  },
+                  itemExtent: 32,
+                  children: [
+                    Text('월'),
+                    Text('화'),
+                    Text('수'),
+                    Text('목'),
+                    Text('금')
+                  ],
+                ),
               ),
-            ));
+
+              // Close the modal
+              CupertinoButton(
+                child: Text('OK'),
+                padding: EdgeInsets.zero,
+                onPressed: () => Navigator.of(ctx).pop(),
+              )
+            ],
+          ),
+        ));
   }
 
   @override
@@ -329,7 +328,13 @@ class __TableState extends State<_Table> {
     //double screen_width = double.infinity;
     //double screen_height = double.infinity;
 
-    List<String> my_lecture = ['기초영어', ];
+    //List<String> my_lecture = ['기초영어', ];
+    //if (context.read<BoxSize>().mlecture.length == 0) {
+    //  String a = "";}
+    // else if (context.read<BoxSize>().mlecture.length == 1) {
+    //String a =  context.read<BoxSize>().mlecture[1];
+    //}
+
 
     return Padding(
       padding: EdgeInsets.fromLTRB(10, 10, 10, 10),
@@ -685,8 +690,9 @@ class __TableState extends State<_Table> {
               ),
             ],
           ),
-
-          ClassBox(lectureName: '',),
+          //LectureBox(), =====> change
+          //List.unmodifiable(elements),
+          ClassBox(lectureName: context.read<BoxSize>().mlecture[1],),
           ClassBox(lectureName: '',),
           ClassBox(lectureName: '',),
           ClassBox(lectureName: '',),
@@ -694,8 +700,8 @@ class __TableState extends State<_Table> {
           // 1
           // 2
           // 3
-          _TableBox(),
-          AddBox(),
+          //_TableBox(),
+          //AddBox(),
         ]),
       ),
     );
@@ -873,21 +879,87 @@ class _TableRowState extends State<TableRow> {
 }
 
 class BoxSize with ChangeNotifier {
-  double _boxwidth = 0;
+  String _selectedLecture = '초기화';
+  double _width = 0; // 고정값
+  double _height = 0;
+  double _horizontal = 0;
+  double _vertical = 0;
+  List _mlecture = ["","",""]; // =====>change
+  // List<Widget> _lectureList = <Widget>[Container(height: 20,width: 20,color: Colors.black,)]; =====>change
 
-  double get boxwidth => _boxwidth;
+  String get selectedLecture => _selectedLecture;
+  double get width => _width;
+  double get height => _height;
+  double get horizontal => _horizontal;
+  double get vertical => _vertical;
+  List get mlecture => _mlecture; // =====>change
+  //List<Widget> get lectureList => _lectureList; // =====>change
 
+  box() {
+
+  }
   add() {
-    _boxwidth = _boxwidth + 10;
+    _mlecture.add(_selectedLecture);
+    print(_mlecture);
+  }
+/*
+  add() {
+    _lectureList.add(
+        Align(
+          alignment: Alignment(2, 2), //set position depend on lecture date and time
+          child:
+          Container(
+            height : 50, //set height depend on lecture duration
+            width : 50, //set width
+            color: Colors.blue,
+
+    ),),);
+    pr(_lectureList);
+    notifyListeners();
+  }
+
+ */
+  delete_all() {
+    _mlecture = [""];
+  }
+  //delete_all(){
+  //  _lectureList = <Widget>[Container(height: 20,width: 20,color: Colors.black,)];
+  //}
+
+  pass(String lecture_name) {
+    _selectedLecture = lecture_name;
     notifyListeners();
   }
 
   pr() {
-    print("hello");
+    print(_selectedLecture);
     notifyListeners();
   }
-}
 
+  setweek(int week) {
+    if (week  == 1) {
+      _horizontal = -0.87;
+    } else if (week  == 2) {
+      _horizontal = -0.405;
+    } else if (week == 3) {
+      _horizontal = 0.065;
+    } else if (week == 4) {
+      _horizontal = 0.53;
+    } else if (week  == 5) {
+      _horizontal = 0.997;
+    }
+    notifyListeners();
+  }
+
+  setothers(double start_time, double end_time) {
+    _vertical = start_time; // 연산추가 필요
+    _height = end_time - start_time; //연산추가 필요
+    print(_vertical);
+    print(_height);
+  }
+  notifyListeners();
+}
+/*
 class _TableBox extends StatefulWidget {
   const _TableBox({Key? key}) : super(key: key);
 
@@ -913,48 +985,10 @@ class __TableBoxState extends State<_TableBox> {
     ]);
   }
 }
-
-/*
-class LectureBox extends StatefulWidget {
-  const LectureBox({Key? key}) : super(key: key);
-
-  @override
-  _LectureBoxState createState() => _LectureBoxState();
-}
-
-class _LectureBoxState extends State<LectureBox> {
-  @override
-  Widget build(BuildContext context) {
-    double screen_width = MediaQuery.of(context).size.width;
-    double screen_height = MediaQuery.of(context).size.height;
-    return Positioned(bottom: 10, right: 10,
-        top: screen_height / 43 + screen_height / 500,
-        left: screen_width / 18 + screen_width / 250 +(screen_width / 250 + screen_width / 5.8) * 0,
-        child: Container(color: Colors.yellow,)
-    );
-  }
-}
 */
 
-class AddBox extends StatefulWidget {
-  const AddBox({Key? key}) : super(key: key);
-
-  @override
-  _AddBoxState createState() => _AddBoxState();
-}
-
-class _AddBoxState extends State<AddBox> {
-  @override
-  Widget build(BuildContext context) {
-
-    return Container(color: Colors.red,
-        width: Provider.of<BoxSize>(context).boxwidth,
-        height: Provider.of<BoxSize>(context).boxwidth);
-
-  }
-}
-
 class DataSearch extends SearchDelegate<String> {
+
   final lectures = [
     "대학글쓰기1",
     "대학글쓰기2",
@@ -966,6 +1000,10 @@ class DataSearch extends SearchDelegate<String> {
     "재무관리"
   ];
   final recentLectures = ["대학글쓰기1", "대학글쓰기2"];
+  final lectureinfo = [["대학글쓰기1",1,3.0,5.0,"가나다","58동 401호"],
+    ["대학글쓰기2",2,9.0,10.0,"라마바","58동 402호"],
+    ["대학영어1",4,2.0,5.0,"사아자","58동 403호"],
+    ["대학영어2",4,2.0,5.0,"차카타파하","58동 404호"]];
 
   @override
   List<Widget> buildActions(BuildContext context) {
@@ -982,23 +1020,7 @@ class DataSearch extends SearchDelegate<String> {
   @override
   Widget buildLeading(BuildContext context) {
     // leading icon on the left of the app bar
-    BoxSize _boxSize = Provider.of<BoxSize>(context);
-    return IconButton(
-        icon: AnimatedIcon(
-          icon: AnimatedIcons.menu_arrow,
-          progress: transitionAnimation,
-        ),
-        onPressed: () {
-          _boxSize.add();
-          close(context, "");
-          // Navigator.pop(context);
-        });
-  }
 
-  @override
-  Widget buildResults(BuildContext context) {
-    // show some result based on the selection
-    BoxSize _boxSize = Provider.of<BoxSize>(context);
     return IconButton(
         icon: AnimatedIcon(
           icon: AnimatedIcons.menu_arrow,
@@ -1006,30 +1028,13 @@ class DataSearch extends SearchDelegate<String> {
         ),
         onPressed: () {
           close(context, "");
-          _boxSize.add();
-          _boxSize.pr();
-
-          /*Stack(
-            children: [_TableBox(),
-              // Positioned(bottom: 10, right: 10,
-              // top: screen_height / 43 + screen_height / 500,
-               //  left: screen_width / 18 + screen_width / 250 +(screen_width / 250 + screen_width / 5.8) * 0,
-                // child:
-                  Container(color: Colors.yellow,)]
-          );
 
           // Navigator.pop(context);
-          */
         });
   }
-
-  //     Scaffold(
-  //     body: Text('hello'),
-  //   );
-  // }
-
   @override
   Widget buildSuggestions(BuildContext context) {
+    BoxSize _boxSize = Provider.of<BoxSize>(context);
     // show when someone searches for something
     final suggestionList = query.isEmpty
         ? recentLectures
@@ -1038,6 +1043,8 @@ class DataSearch extends SearchDelegate<String> {
     return ListView.builder(
       itemBuilder: (context, index) => ListTile(
         onTap: () {
+          _boxSize.pass(suggestionList[index]);
+          _boxSize.pr();
           showResults(context);
         },
         leading: Icon(Icons.circle),
@@ -1045,7 +1052,7 @@ class DataSearch extends SearchDelegate<String> {
           text: TextSpan(
               text: suggestionList[index].substring(0, query.length),
               style:
-                  TextStyle(color: Colors.black, fontWeight: FontWeight.bold),
+              TextStyle(color: Colors.black, fontWeight: FontWeight.bold),
               children: [
                 TextSpan(
                     text: suggestionList[index].substring(query.length),
@@ -1056,8 +1063,159 @@ class DataSearch extends SearchDelegate<String> {
       itemCount: suggestionList.length,
     );
   }
+
+
+  @override
+  Widget buildResults(BuildContext context) {
+    // show some result based on the selection
+    BoxSize _boxSize = Provider.of<BoxSize>(context);
+    final String lecName = context.read<BoxSize>().selectedLecture;
+    List selectedinfo = ["","","","",""];
+
+    for (List each in lectureinfo) {
+      if (each[0] == lecName) {
+        int lec_week = each[1];
+        /*
+        if (lec_week == 1) {
+          selectedinfo[0] = '월요일';
+        } else if (lec_week == 2) {
+          selectedinfo[0] = '화요일';
+        } else if (lec_week == 3) {
+          selectedinfo[0] = '수요일';
+        } else if (lec_week == 4) {
+          selectedinfo[0] = '목요일';
+        } else if (lec_week == 5) {
+          selectedinfo[0] = '금요일';
+        }
+*/
+        selectedinfo[0] = lec_week;
+        double lec_start_time = each[2];
+        selectedinfo[1] = lec_start_time;
+        double lec_end_time = each[3];
+        selectedinfo[2] = lec_end_time;
+        String professor_name = each[4];
+        selectedinfo[3] = professor_name;
+        String place = each[5];
+        selectedinfo[4] = place;
+
+      };
+    };
+
+    return ListView(
+      padding: EdgeInsets.fromLTRB(0, 10, 0, 0),
+      children: [
+        ListTile(
+          title: Text(lecName),
+          subtitle: Column(
+            children: [
+              Row(
+                children: [
+                  Text(selectedinfo[3]),
+                  SizedBox(width: 5,),
+                  Text(selectedinfo[4]),
+                ],
+              ),
+              Row(
+                  children:[
+                    Text(selectedinfo[0].toString()),
+                    SizedBox(width: 5,),
+                    Text(selectedinfo[1].toString()),
+                    Text("-"),
+                    Text(selectedinfo[2].toString()),
+                  ]
+              ),
+            ],
+          ),
+          trailing: IconButton(
+              icon: AnimatedIcon(
+                icon: AnimatedIcons.event_add,
+                progress: transitionAnimation,),
+              onPressed: () {
+                close(context, "bye");
+                _boxSize.setweek(selectedinfo[0]);
+                _boxSize.pr();
+                _boxSize.add();
+                _boxSize.setothers(selectedinfo[1], selectedinfo[2]);
+
+                //print(context.read<BoxSize>().lectureList);
+                print(context.read<BoxSize>().mlecture);
+              }),
+        ),
+        ListTile(
+          title: Text(lecName),
+          subtitle: Column(
+            children: [
+              Row(
+                children: [
+                  Text(selectedinfo[3]),
+                  SizedBox(width: 5,),
+                  Text(selectedinfo[4]),
+                ],
+              ),
+              Row(
+                  children:[
+                    Text(selectedinfo[0].toString()),
+                    SizedBox(width: 5,),
+                    Text(selectedinfo[1].toString()),
+                    Text("-"),
+                    Text(selectedinfo[2].toString()),
+                  ]
+              ),
+            ],
+          ),
+          trailing: IconButton(
+              icon: AnimatedIcon(
+                icon: AnimatedIcons.event_add,
+                progress: transitionAnimation,),
+              onPressed: () {
+                close(context, "bye");
+                _boxSize.setweek(selectedinfo[0]);
+                _boxSize.pr();
+                _boxSize.add();
+                _boxSize.setothers(selectedinfo[1], selectedinfo[2]);
+
+                //print(context.read<BoxSize>().lectureList);
+                print(context.read<BoxSize>().mlecture);
+              }),
+        ),
+
+      ],
+    );
+  }
 }
 
+/*
+class LectureBox extends StatefulWidget {
+  const LectureBox({Key? key}) : super(key: key);
+
+  @override
+  _LectureBoxState createState() => _LectureBoxState();
+}
+
+class _LectureBoxState extends State<LectureBox> {
+  @override
+  List<Widget> getListFiles() {
+    List<Widget> list = context.read<BoxSize>().lectureList;
+    return list;
+  }
+  Widget build(BuildContext context) {
+    //context.read<BoxSize>().lectureList.forEach((lec) => );
+    return// Stack(
+      //children: [
+        //  context.read<BoxSize>().lectureList.forEach((lec) => lec.createElement())
+     // ],
+   // );
+
+    Stack(
+      children: List.unmodifiable(() sync* {
+        yield* getListFiles();
+      }()
+      ),
+    );
+  }
+}
+
+*/
 
 //참고용 widget
 class ClassBox extends StatefulWidget {
@@ -1072,9 +1230,9 @@ class ClassBox extends StatefulWidget {
 class _ClassBoxState extends State<ClassBox> {
   List<Widget> testList = <Widget>[
     Container(
-      width : 50,
-      height : 50,
-      color : Colors.black
+        width : 50,
+        height : 50,
+        color : Colors.black
     ),
   ];
 
@@ -1087,10 +1245,8 @@ class _ClassBoxState extends State<ClassBox> {
     double startTime = 1;
     return startTime;
   }
-  double findEndTime(String lectureName){
-    double endTime = 3;
-    return endTime;
-  }
+
+
   double ClassTime(String lectureName){
 
     return 31;
@@ -1098,15 +1254,26 @@ class _ClassBoxState extends State<ClassBox> {
 
   @override
   Widget build(BuildContext context) {
-    return Align(
+    double screen_width = MediaQuery.of(context).size.width;
+    double screen_height = MediaQuery.of(context).size.height;
+    double hor = context.read<BoxSize>().horizontal;
+    double ver = context.read<BoxSize>().vertical;
+    double hei = context.read<BoxSize>().height;
 
-      alignment: Alignment(-0.41, -0.326), //set position depend on lecture date and time
-      child: Container(
-        height : ClassTime(widget.lectureName), //set height depend on lecture duration
-        width : 74, //set width
-        color: Colors.blue,
-      ),
-    );
+    return
+      Align(
+        alignment: Alignment(hor, -0.326), //set position depend on lecture date and time
+        child: GestureDetector(
+          child: Container(
+            height : ClassTime(widget.lectureName), //set height depend on lecture duration
+            width : (screen_width - 41) / 5, //set width
+            color: Colors.blue,
+          ),
+          onTap:() {
+            print(hor);
+          },
+        ),
+      );
   }
 }
 
